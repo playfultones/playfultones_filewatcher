612b17bff3af3119ae4ac46bcfc3744aa0360100
Bence Kovács <23636204+githappens@users.noreply.github.com>
2025-07-28 11:16:20 +0200

    Fix dependencies in module declaration and update includes

--- playfultones_filewatcher.h
+++ playfultones_filewatcher.h
@@ -6,15 +6,16 @@ name:             Playful Tones File Watcher
 description:      JUCE integration for file watching capabilities via efsw.
 website:          https://playfultones.com
 license:          MIT
-dependencies:     juce_core
+dependencies:     juce_events
 END_JUCE_MODULE_DECLARATION
 */
 #pragma once
 #define PLAYFULTONES_FILEWATCHER_H_INCLUDED
 
 #include <efsw/efsw.hpp>
-#include <juce_core/juce_core.h>
+#include <juce_events/juce_events.h>
 
-#include "src/FileWatcher.h"
 #include "src/enums/FileAction.h"
 #include "src/interfaces/FileWatcherListener.h"
+
+#include "src/FileWatcher.h"
